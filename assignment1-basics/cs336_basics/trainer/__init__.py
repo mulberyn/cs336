2d7e7d1a2e8f1e3c2442b392d69578cebc15a514
mulberyn <mulberyn@gmail.com>
2026-07-29 22:25:29 +0800

    feat: implement model checkpoint save/load

--- assignment1-basics/cs336_basics/trainer/__init__.py
+++ assignment1-basics/cs336_basics/trainer/__init__.py
@@ -7,11 +7,17 @@ from .utils import (
 )
 from .adamw import AdamW
 from .data_loading import data_loading
+from .checkpoint import (
+    save_checkpoint,
+    load_checkpoint
+)
 
 __all__ = [
     "cross_entropy",
     "AdamW",
     "get_lr_cosine_schedule",
     "gradient_clipping",
-    "data_loading"
+    "data_loading",
+    "save_checkpoint",
+    "load_checkpoint"
 ]
---
+++ assignment1-basics/cs336_basics/trainer/checkpoint.py
@@ -0,0 +1,25 @@
+import torch
+
+def save_checkpoint(
+    model: torch.nn.Module,
+    optimizer: torch.optim.Optimizer,
+    iteration: int,
+    out,
+):
+    checkpoint = {
+        'model': model.state_dict(),
+        'optimizer': optimizer.state_dict(),
+        'iteration': iteration
+    }
+    torch.save(checkpoint, out)
+    
+
+def load_checkpoint(
+    src,
+    model: torch.nn.Module,
+    optimizer: torch.optim.Optimizer
+) -> int:
+    checkpoint = torch.load(src)
+    model.load_state_dict(checkpoint['model'])
+    optimizer.load_state_dict(checkpoint['optimizer'])
+    return checkpoint['iteration']
